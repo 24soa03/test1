7d371073f76de4acff419ee6cae4cba63f55ef89
24soa03 <soa2403@mail.ru>
2024-03-21 19:08:06 +0300

    123

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число ");
+        int a = sc.nextInt();
+        System.out.println("Введите второе число ");
+        int b = sc.nextInt();
+        System.out.println("Введите операцию ");
+        char d = sc.next().charAt(0);
+
+        if (d == '+') {
+            System.out.println("Ваше значение: " + (a + b));
+        } else if (d == '-') {
+            System.out.println("Ваше значение: " + (a - b));
+        } else if (d == '*') {
+            System.out.println("Ваше значение: " + (a * b));
+        } else if (d == '/') {
+            System.out.println("Ваше значение: " + (a / b));
+        }
+
+    }
+}
